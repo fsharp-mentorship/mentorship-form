54416a47f9992ba90a5f8b7be55af92c79ed0206
Michael Oyer <code@michaeloyer.com>
2022-11-13 13:56:35 -0500

    Working hello-world

--- netlify/functions/hello-world/hello-world.ts
+++ netlify/functions/hello-world/hello-world.ts
@@ -1,8 +1,16 @@
 import { Handler } from '@netlify/functions'
+import { Client, Create, Collection } from 'faunadb'
+
 
 export const handler: Handler = async (event, context) => {
   const body = JSON.parse(event.body ?? '{"name":"stranger"}')
 
+  console.log(event)
+
+  // const client = new Client({ secret: process.env.FAUNADB_SERVER_SECRET ?? "" })
+
+  // await client.query(Create(Collection('testing', JSON.stringify(body))))
+  
   return {
     statusCode: 200,
     body: JSON.stringify({
