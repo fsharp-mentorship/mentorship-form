8b92f64228c32174190e3923b565861548f91b12
Michael Oyer <code@michaeloyer.com>
2022-11-30 01:23:15 -0500

    Read FaunaDb Collection name from Enviornment Variable

--- functions/submit-applicant.ts
+++ functions/submit-applicant.ts
@@ -3,12 +3,12 @@ import { Client, Create, Collection } from 'faunadb'
 
 // @ts-ignore Unused Variable
 export const handler: Handler = async (event, context) => {
-  const data = JSON.parse(event.body ?? "")
+  const data = JSON.parse(event.body ?? '')
 
-  const client = new Client({ secret: process.env.FAUNADB_SERVER_SECRET ?? "" })
+  const client = new Client({ secret: process.env.FAUNADB_SERVER_SECRET ?? '' })
 
   await client.query(
-    Create(Collection('testing'), {data: data} )
+    Create(Collection(process.env.FAUNADB_APPLICANT_COLLECTION ?? ''), {data: data} )
   )
   
   return {
